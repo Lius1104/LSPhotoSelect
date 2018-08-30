6c10e03c0efcbc542954df6df35642ba6b9f089e
Lius <15093319898@163.com>
2018-08-30 09:18:46 +0800

    创建项目

---
+++ LSPhotoSelect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSPhotoSelect
+//
+//  Created by Shuang Lau on 2018/8/30.
+//  Copyright © 2018年 Shuang Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSPhotoSelect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSPhotoSelect
+//
+//  Created by Shuang Lau on 2018/8/30.
+//  Copyright © 2018年 Shuang Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
